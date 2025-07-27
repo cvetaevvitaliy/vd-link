583ba0ad543c4037ab25b04f8fd5a0d0504deae0
Vitaliy Nimych <vitaliy.nimych@gmail.com>
2025-07-27 16:27:37 +0300

    refactor project: add GS & drone

---
+++ drone/src/main.c
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/**
+ * Copyright (C) 2025 Vitaliy N <vitaliy.nimych@gmail.com>
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("hello world\n");
+    return 0;
+}
