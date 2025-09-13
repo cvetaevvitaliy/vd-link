eb0ebe7e8d926126735806d8f5d0863389aefce4
vitaliy <cvetaevvitaliy@gmail.com>
2025-09-13 19:36:33 +0300

    tune lvgl config

--- gs/src/lv_conf.h
+++ gs/src/lv_conf.h
@@ -969,7 +969,7 @@
 #define LV_USE_VECTOR_GRAPHIC  1
 
 /** Enable ThorVG (vector graphics library) from the src/libs folder */
-#define LV_USE_THORVG_INTERNAL 1
+#define LV_USE_THORVG_INTERNAL 0
 
 /** Enable ThorVG by assuming that its installed and linked to the project */
 #define LV_USE_THORVG_EXTERNAL 0
